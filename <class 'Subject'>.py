0e0089f07914b47a0aa7aa50b6e974e365c165ad
Mohamed Amine <100172875+M0-Amine@users.noreply.github.com>
2023-04-22 00:18:07 +0000

    Update <class 'Subject'>.py

--- <class 'Subject'>.py
+++ <class 'Subject'>.py
@@ -1,9 +1,10 @@
 from typing import Union
-
 Subject = type("Subject")
 subject_id: int
 student_id: int
 assessment_type: str
+status: str
+rate: float
 
 days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 dic_delay = {range(0, 1): "Present", range(1, 6): "Late", range(6, 180): "Absent"}
@@ -11,6 +12,13 @@ assessment_types = ["TD", "TP", "Quizz", "Exam"]
 genders = ["Male", "Female"]
 groups = ["A", "B", "C", "D"]
 
+# IDs generator
+def id_gen():
+    id = 0
+    while True:
+        id += 1
+        yield id
+generator = id_gen()
 
 class Attendance:
     
@@ -18,6 +26,7 @@ class Attendance:
         self.total_minutes = total_mintes
         self.late_minutes = late_minutes
         self.day = day
+        self.id = next(generator)
         self.status = self.get_attendance_status()
     
     def get_attendance_status(self) -> str:
@@ -40,10 +49,13 @@ class Attendance:
     def get_status(self):
         return self.status
     
+    def get_attendance_id(self):
+        return self.id
+    
     def set_total_minutes(self, totalmin: int):
         if totalmin < 0:
             raise ValueError("number of minutes cannot be negative")
-            
+        
         self.total_minutes = totalmin
           
     def set_late_minutes(self, latemin: int):
@@ -60,15 +72,17 @@ class Attendance:
             raise ValueError("invalid day name")
         self.day = day
         
+    def set_attendance_id(self, new_id: int):
+        self.get_attendance_id = new_id
+         
 
 class Assessment:
     
-    def __init__(self, id: int, assesment_type: str, grade: int, weight: float) -> None:
+    def __init__(self, assesment_type: str, grade: int, weight: float) -> None:
+        self.id = next(generator)
         self.assessment_type = assesment_type
         self.grade = grade
-        self.id = id
         self.weight = weight
-        
         
     # Getters and setters
     def get_type(self):
@@ -77,12 +91,11 @@ class Assessment:
     def get_grade(self):
         return self.grade
     
-    def set_type(self, type: str):
-        
-        if not type in assessment_types:
+    def set_type(self, assessment_type: str):
+        if not assessment_type in assessment_types:
             raise ValueError("invalid assessment type")
         
-        self.type = type
+        self.assessment_type = assessment_type
         
     def set_grade(self, grade: int):
         if not grade in range(0, 20):
@@ -90,16 +103,16 @@ class Assessment:
         
         self.grade = grade
         
-    def get_id(self):
+    def get_assessment_id(self):
         return self.id
-    
+  
     
 class Student:
     
-    def __init__(self, id: int, name: str, gender: str, group: str) -> None:
+    def __init__(self, name: str, gender: str, group: str) -> None:
+        self.id = next(generator)
         self.name = name
         self.gender = gender
-        self.id = id
         self.group = group
         self.subjects: list[Subject] = []
         self.attendances: dict[subject_id: int, list[Attendance]] = {}
@@ -110,9 +123,21 @@ class Student:
         if not subject in self.subjects:
             self.subjects.append(subject)
             
+    def final_grade(self):
+        grade = 0
+        finale_rate = 0
+        for subject in self.subjects:
+            for assessment in self.assessments[subject]:
+                grade += assessment.get_grade() * subject.weights[assessment.get_type()]
+                
+            for attendance in self.attendances[subject]:
+                finale_rate += subject.rates[attendance.get_attendance_status()] / len(self.attendances[subject])
+        grade *= finale_rate
+        return grade
+            
             
     # Getters and Setters
-    def get_name(self):
+    def get_student_name(self):
         return self.name
         
     def get_gender(self):
@@ -121,7 +146,10 @@ class Student:
     def get_group(self):
         return self.group
     
-    def set_name(self, newName: str):
+    def get_student_id(self):
+        return self.id
+    
+    def set_student_name(self, newName: str):
         if newName == "":
             raise ValueError("Invalid Name. Name cannot be empty.")
         self.name = newName
@@ -132,7 +160,6 @@ class Student:
         self.gender = newGend
         
     def set_group(self, newGroup):
-        
         if not newGroup in groups:
             raise ValueError(f"Invalid group. Group must be one of {groups}.")       
         self.group = newGroup
@@ -166,15 +193,16 @@ class Student:
             print(f"{subject} Attendance:")
             for attendance in sorted(self.attendance[subject], key=lambda att: days_of_week.index(att.day)):
                 print(f"  - {attendance.day}: {attendance.total_minutes} minutes (Late: {attendance.late_minutes} minutes)")
-          
+         
         
 class Subject:
     
-    def __init__(self, id: int, name: str) -> None:    
-        self.id = id
+    def __init__(self, name: str) -> None:    
+        self.id = next(generator)
         self.name = name
         self.weights: dict[assessment_type: str, float] = {}
         self.participants: list[Student] = []
+        self.rates: dict[status: str, rate: float] = {}
     
     def add_student(self, student: Student) -> None:  # this is an observer hh
         """ Adds Student object to participants list AND calls add_subject """
@@ -209,7 +237,7 @@ class Subject:
                     
         elif type(studentObj_or_id) == int:
             for student in self.participants:
-                if student.id == studentObj_or_id:
+                if student.get_student_id() == studentObj_or_id:
                     for assessment in student.assessments[self.id]:
                         if assessment.get_id() == assessment_id:
                             assessment.set_grade(new_grade)
@@ -218,7 +246,7 @@ class Subject:
         """ Removes the specified assessment from the assessments dict of every student """
         for student in self.participants:
             for assessment in student.assessments[self.id]:
-                if assessment.id == assessment_id:
+                if assessment.get_assessment_id() == assessment_id:
                     student.assessments[self.id].remove(assessment)
     
     def modify_attendance_by_id(self, studentObj_or_id: Union[Student, int], attendance_id: int, new_late_minutes: int) -> None:
@@ -234,5 +262,19 @@ class Subject:
                     for attendance in student.attendances[self.id]:
                         if attendance.id == attendance_id:
                             attendance.set_late_minutes(new_late_minutes)
-                                    
-
+                            
+        
+    # Getters and Setters
+    def get_subject_id(self):
+        return self.id
+    
+    def get_subject_name(self):
+        return self.name
+    
+    def set_subject_name(self, new_name: str):
+        self.name = new_name
+    
+    def set_rates(self, *args: tuple[float]):
+        for status, rate in enumerate(dic_delay.values(), args):
+            self.rates[status] = rate
+   
